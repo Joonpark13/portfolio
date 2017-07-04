d3f661d8cf3b45890bc3453efaffd20efa2add13
Joon Park <joon.park13@gmail.com>
2017-07-04 17:36:32 -0500

    Add React App using create-react-app
    Currently, the site only shows a title page with a name.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import TitlePage from './TitlePage';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main-column">
+        <TitlePage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TitlePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/TitlePage.css';
+
+class TitlePage extends Component {
+    render() {
+        return (
+            <div id="title-page">
+                <h1 id="name">Joon Park</h1>
+            </div>
+        );
+    }
+}
+
+export default TitlePage;
