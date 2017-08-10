f5c0829efabe4bcf29a7c96e6cace4d1cf53e6e1
Joon Park <joon.park13@gmail.com>
2017-08-09 23:15:05 -0500

    Linting
    Replace html apostrophes with escape codes

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,17 @@ const App = () => (
     <div id="title-page" className="page">
       <div id="title-content">
         <StripeCard>
-          <h2 id="name-line">Hi. I'm Joon.</h2>
+          <h2 id="name-line">Hi. I&apos;m Joon.</h2>
         </StripeCard>
 
         <div className="content-width-wrapper">
           <p className="intro-text">
-            <span className="bold">I'm a web developer </span>
+            <span className="bold">I&apos;m a web developer </span>
             focused on beautiful design and fine-tuned user experiences.
           </p>
           <p className="intro-text">
-            I currently study physics, music composition, and computer science at Northwestern University.
+            I currently study physics, music composition, and computer science at Northwestern
+            University.
           </p>
         </div>
       </div>
@@ -51,15 +52,15 @@ const App = () => (
 
         <div id="about-text-wrapper">
           <p className="page-text">
-            My name is Joon Park  and I'm a web developer raised in Ithaca NY.
-            I'm currently living in the Chicagoland area finishing up my last year at Northwestern
-            University, where I study physics, music composition, and computer science.
+            My name is Joon Park  and I&apos;m a web developer raised in Ithaca NY.
+            I&apos;m currently living in the Chicagoland area finishing up my last year at
+            Northwestern University, where I study physics, music composition, and computer science.
           </p>
           <p className="page-text">
             Physics research is actually how I learned to code - but I found
             out I enjoy the design and problem solving of full stack development even more.
-            As a young developer I'm excited to continue feeding my insatiable appetite for learning.
-            Check out some of my past projects below!
+            As a young developer I&apos;m excited to continue feeding my insatiable appetite for
+            learning. Check out some of my past projects below!
           </p>
         </div>
       </div>
@@ -74,7 +75,7 @@ const App = () => (
           <p className="project-summary-text">
             <a href="https://serif.nu">Serif.nu</a> is a course scheduling web app designed to
             help Northwestern students plan their class registration more easily and quickly.
-            Motivated by frustrations in using Northwestern's own course registration system,
+            Motivated by frustrations in using Northwestern&apos;s own course registration system,
             this project served as my introduction to JavaScript and web development in general.
             The first version (created using <span className="bold">Flask</span> and
             <span className="bold"> PostgreSQL</span>) was released Fall 2015, and a new version
@@ -100,7 +101,7 @@ const App = () => (
           <p className="project-summary-text">
             <a href="https://censusreporter.com">Census Reporter</a> aims to make the access of
             U.S. census data easier and simpler. This was my first experience diving into an
-            established code base. Since joining the team, I've covered the full stack while
+            established code base. Since joining the team, I&apos;ve covered the full stack while
             implementing full-text search in the Census Reporter API, designing a search results
             page, generating sitemaps, and making data download files more human readable.
           </p>
@@ -109,7 +110,7 @@ const App = () => (
             <span className="bold"> Flask</span>, and <span className="bold">PostgreSQL</span>.
           </p>
           <p className="project-summary-text">
-            Through the Census Reporter project, I'm also affiliated with the Northwestern
+            Through the Census Reporter project, I&apos;m also affiliated with the Northwestern
             University <a href="https://knightlab.northwestern.edu/">Knight Lab</a>.
           </p>
           <div className="project-summary-icons">
@@ -159,12 +160,12 @@ const App = () => (
             was designed to showcase the various
             aspects of my life. The challenge was twofold: I needed a site design that would
             showcase my skills and reflect my personality, and I wanted to fully utilize the power
-            of HTML and CSS3. I was able to use minimal JavaScript to build this site, allowing me to
-            focus on improving my HTML/CSS skills.
+            of HTML and CSS3. I was able to use minimal JavaScript to build this site, allowing me
+            to focus on improving my HTML/CSS skills.
           </p>
           <p className="project-summary-text">
-            I quickly realized that I needed a more developer-centric portfolio website, and redesigned the site
-            incorporating elements from <a href="https://material.io/">Material Design</a>.
+            I quickly realized that I needed a more developer-centric portfolio website, and
+            redesigned the site incorporating elements from <a href="https://material.io/">Material Design</a>.
           </p>
           <div className="project-summary-icons">
             <IconButton>
@@ -182,8 +183,8 @@ const App = () => (
     <ContentPage title="Contact">
       <div id="contact-content">
         <p className="page-text">
-          Whether it's a job opportunity or a sci-fi book recommendation (I love those),
-          I'm down to chat about anything!
+          Whether it&apos;s a job opportunity or a sci-fi book recommendation (I love those),
+          I&apos;m down to chat about anything!
         </p>
 
         <div id="icons">
