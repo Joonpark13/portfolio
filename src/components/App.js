36b6277050f39f156797cd2c032a5d41fca72ced
Joon Park <joon.park13@gmail.com>
2017-08-23 20:42:55 -0500

    Grammatical changes

--- src/components/App.js
+++ src/components/App.js
@@ -26,10 +26,6 @@ const App = () => (
             <span className="bold">I&apos;m a web developer </span>
             focused on beautiful design and great user experiences.
           </p>
-          <p className="intro-text">
-            I currently study physics, music composition, and computer science at Northwestern
-            University.
-          </p>
         </div>
       </div>
 
@@ -55,13 +51,13 @@ const App = () => (
 
         <div id="about-text-wrapper">
           <p className="page-text">
-            Welcome! I&apos;m Joon and I&apos;m a web developer based in the Chicagoland area.
-            I&apos;m currently finishing up my last year at Northwestern University, where I study
+            Welcome! I&apos;m Joon and I&apos;m a web developer based in the Chicago area.
+            I&apos;m finishing up my last year at Northwestern University, where I study
             physics, music composition, and computer science.
           </p>
           <p className="page-text">
             Physics research is actually how I started coding, but I soon found that
-            I enjoy the design and problem solving of web development even more.
+            I enjoyed the design and problem-solving of web development even more.
             As a young developer I&apos;m excited to continue feeding my insatiable appetite for
             learning. Check out some of my past projects below!
           </p>
@@ -78,17 +74,19 @@ const App = () => (
           <p className="project-summary-text">
             <a href="https://serif.nu">Serif.nu</a> is a course scheduling web app designed to
             help Northwestern students plan their course schedules more easily and quickly.
-            Motivated by frustrations in using Northwestern&apos;s own course registration system,
-            this project served as my introduction to JavaScript and web development in general.
-            The first version (created using <span className="bold">Flask</span> and
+            The project aimed to address frustrations in using Northwestern&apos;s own
+            course registration system, but it became my introduction to JavaScript and
+            web development in general.  The first version
+            (created using <span className="bold">Flask</span> and
             <span className="bold"> PostgreSQL</span>) was released Fall 2015, and a new version
-            (rebuild from scratch using <span className="bold">React</span> and
+            (rebuilt from scratch using <span className="bold">React</span> and
             <span className="bold"> Node.js</span>) was released Winter 2017.
           </p>
           <p className="project-summary-text">
-            Serif.nu served not only as my gateway into web development but also taught me about
-            what it takes to turn an idea into a product under tight schedules. It even gave me
-            opportunities to learn skills I never thought I would, such as how to market a product.
+            Serif.nu served not only as my gateway into web development, but also taught me about
+            what it takes to turn an idea into a product under tight schedules. I even learned a few
+            non-technical skills along the way, like how to market a product and how to pitch an
+            idea to potential partners.
           </p>
           <p className="project-summary-text highlight">
             Serif.nu was the first place winner of the Northwestern IEEE Project Showcase.
@@ -102,19 +100,15 @@ const App = () => (
           
         <ProjectDisplay title="Census Reporter" img={CRScreenshot} large>
           <p className="project-summary-text">
-            <a href="https://censusreporter.com">Census Reporter</a> aims to make the access of
-            U.S. census data easier and simpler. This was my first experience diving into an
-            established code base. Since joining the team, I&apos;ve covered the full stack while
+            <a href="https://censusreporter.org">Census Reporter</a> aims to simplify access to
+            U.S. census data. This was my first experience diving into an
+            established code base. Since joining the team, I&apos;ve covered the full stack by
             implementing full-text search in the Census Reporter API, designing a search results
-            page, generating sitemaps, and making data download files more human readable.
+            page, generating sitemaps, and making data download files more human-friendly.
           </p>
           <p className="project-summary-text">
-            Census Reporter is built using <span className="bold">Django</span>,
+            Census Reporter was built using <span className="bold">Django</span>,
             <span className="bold"> Flask</span>, and <span className="bold">PostgreSQL</span>.
-          </p>
-          <p className="project-summary-text">
-            Through the Census Reporter project, I&apos;m also affiliated with the Northwestern
-            University <a href="https://knightlab.northwestern.edu/">Knight Lab</a>.
           </p>
           <div className="project-summary-icons">
             <IconButton href="https://github.com/censusreporter/censusreporter">
@@ -128,24 +122,24 @@ const App = () => (
       </div>
 
       <div className="small-project-display">
-        <ProjectDisplay title="RightStudent & ASM">
+        <ProjectDisplay title="Scholarships.com">
           <p className="project-summary-text">
-            While interning for Scholarships.com, I redesigned their two affiliate sites: RightStudent
-            and ASM Omnichannel, as well as a few pages on Scholarships.com. Many of the pages I worked
-            on was in need of a fresh, modern redesign. Focus was placed on the pages being seamlessly
-            responsive and extremely intuitive. Contact me for more details.
+            While interning for Scholarships.com, I overhauled their two affiliate sites: RightStudent
+            and ASM Omnichannel, as well as redesigned a few pages on Scholarships.com. Many of the
+            pages I worked on needed a fresh, modern redesign. Focus was placed on seamlessly
+            responsive layouts and extremely intuitive page designs. Contact me for more details.
           </p>
         </ProjectDisplay>
 
         <ProjectDisplay title="Superhero Fueling" img={SuperheroScreenshot}>
           <p className="project-summary-text highlight">
-            A way to encourage children to eat better.
+            Encouraging children to eat better.
           </p>
           <p className="project-summary-text">
             Superhero Fueling is an app for children that gameifies their eating habits and
-            encourages them to eat more healthily. As they or their parents enter the foods
-            they eat into the app, the superhero will fly and save the day or fall from
-            the sky depending on how healthy their choice of foods were.
+            encourages them to eat more healthily. As they or their parents input the foods
+            they eat into the app, the superhero will either fly - and save the day - or fall,
+            depending on how healthy their choice of foods was.
           </p>
           <p className="project-summary-text">
             This app was built in the second five weeks of the Northwestern University
@@ -167,7 +161,7 @@ const App = () => (
 
         <ProjectDisplay title="JoonPark.me (v1)">
           <p className="project-summary-text">
-            The previous version of this website was designed to showcase the various
+            The previous version of this website was designed to encompass the various
             aspects of my life. The challenge was twofold: I needed a site design that would
             showcase my skills and reflect my personality, and I wanted to fully utilize the power
             of HTML and CSS3. I forced myself to use minimal JavaScript to build the site, allowing me
@@ -175,7 +169,7 @@ const App = () => (
           </p>
           <p className="project-summary-text">
             I quickly realized that I needed a more developer-centric portfolio website, and
-            redesigned the site as you see here incorporating elements from <a href="https://material.io/">Material Design</a>.
+            redesigned the site as seen here incorporating elements from <a href="https://material.io/">Material Design</a>.
           </p>
           <div className="project-summary-icons">
             <IconButton href="https://github.com/Joonpark13/joonpark.me-OLD">
@@ -193,7 +187,7 @@ const App = () => (
     <ContentPage title="Contact">
       <div id="contact-content">
         <p className="page-text">
-          Whether it&apos;s a job opportunity or a sci-fi book recommendation (I love those),
+          Whether it&apos;s a job opportunity or a sci-fi book (I love those) recommendation,
           I&apos;m down to chat about anything!
         </p>
 
