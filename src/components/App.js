e6ccd995fccacc7caa3bd4c7f2d10215a1ed4f2c
Joon Park <joon.park13@gmail.com>
2017-09-09 17:07:37 -0400

    Implement new project display layout
    Previously, the project displays on large screens (1024px and wider) consisted of a large project displays section followed by a small project displays section below it. Now the section shows both of them side by side. This helps the visual presentation of the sections when one of the small project displays are open.
    
    One of the lines of css required to implement this did not compile even though the usage of calc was correct syntactically. The content width is currently hard-coded in until a fix is pushed out by the offending package.

--- src/components/App.js
+++ src/components/App.js
@@ -66,120 +66,139 @@ const App = () => (
     </div>
 
     <ContentPage title="Projects">
-      <div className="large-project-display">
-        <ProjectDisplay title="Serif.nu" img={SerifScreenshot} large>
-          <p className="project-summary-text highlight">
-            Simple. Fast. Visual. Course scheduling for Northwestern Unviersity.
-          </p>
-          <p className="project-summary-text">
-            <a href="https://serif.nu">Serif.nu</a> is a course scheduling web app designed to
-            help Northwestern students plan their course schedules more easily and quickly.
-            The project aimed to address frustrations in using Northwestern&apos;s own
-            course registration system, but it became my introduction to JavaScript and
-            web development in general.  The first version
-            (created using <span className="bold">Flask</span> and
-            <span className="bold"> PostgreSQL</span>) was released Fall 2015, and a new version
-            (rebuilt from scratch using <span className="bold">React</span> and
-            <span className="bold"> Node.js</span>) was released Winter 2017.
-          </p>
-          <p className="project-summary-text">
-            Serif.nu served not only as my gateway into web development, but also taught me about
-            what it takes to turn an idea into a product under tight schedules. I even learned a few
-            non-technical skills along the way, like how to market a product and how to pitch an
-            idea to potential partners.
-          </p>
-          <p className="project-summary-text highlight">
-            Serif.nu was the first place winner of the Northwestern IEEE Project Showcase.
-          </p>
-          <div className="project-summary-icons">
-            <IconButton href="https://github.com/Joonpark13/serif.nu">
-              <span className="fa fa-github"></span>
-            </IconButton>
-          </div>
-        </ProjectDisplay>
-          
-        <ProjectDisplay title="Census Reporter" img={CRScreenshot} large>
-          <p className="project-summary-text">
-            <a href="https://censusreporter.org">Census Reporter</a> aims to simplify access to
-            U.S. census data. Since joining the team, I&apos;ve covered the full stack by
-            implementing full-text search in the Census Reporter API, designing a search results
-            page, generating sitemaps, and making data download files more human-friendly.
-          </p>
-          <p className="project-summary-text">
-            Census Reporter was built using <span className="bold">Django</span>,
-            <span className="bold"> Flask</span>, and <span className="bold">PostgreSQL</span>.
-          </p>
-          <div className="project-summary-icons">
-            <IconButton href="https://github.com/censusreporter/censusreporter">
-              <span className="fa fa-github"></span>
-            </IconButton>
-            <IconButton href="https://github.com/censusreporter/census-api">
-              <span className="fa fa-github"></span>
-            </IconButton>
-          </div>
-        </ProjectDisplay>
-      </div>
+      <div id="projects-content">
+        <div className="large-project-display">
+          <ProjectDisplay title="Serif.nu" img={SerifScreenshot} large>
+            <p className="project-summary-text highlight">
+              Simple. Fast. Visual. Course scheduling for Northwestern Unviersity.
+            </p>
+            <p className="project-summary-text">
+              <a href="https://serif.nu">Serif.nu</a> is a course scheduling web app designed to
+              help Northwestern students plan their course schedules more easily and quickly.
+              The project aimed to address frustrations in using Northwestern&apos;s own
+              course registration system, but it became my introduction to JavaScript and
+              web development in general.  The first version
+              (created using <span className="bold">Flask</span> and
+              <span className="bold"> PostgreSQL</span>) was released Fall 2015, and a new version
+              (rebuilt from scratch using <span className="bold">React</span> and
+              <span className="bold"> Node.js</span>) was released Winter 2017.
+            </p>
+            <p className="project-summary-text">
+              Serif.nu served not only as my gateway into web development, but also taught me about
+              what it takes to turn an idea into a product under tight schedules. I even learned a few
+              non-technical skills along the way, like how to market a product and how to pitch an
+              idea to potential partners.
+            </p>
+            <p className="project-summary-text highlight">
+              Serif.nu was the first place winner of the Northwestern IEEE Project Showcase.
+            </p>
+            <div className="project-summary-icons">
+              <IconButton href="https://github.com/Joonpark13/serif.nu">
+                <span className="fa fa-github"></span>
+              </IconButton>
+            </div>
+          </ProjectDisplay>
+            
+          <ProjectDisplay title="Census Reporter" img={CRScreenshot} large>
+            <p className="project-summary-text">
+              <a href="https://censusreporter.org">Census Reporter</a> aims to simplify access to
+              U.S. census data. Since joining the team, I&apos;ve covered the full stack by
+              implementing full-text search in the Census Reporter API, designing a search results
+              page, generating sitemaps, and making data download files more human-friendly.
+            </p>
+            <p className="project-summary-text">
+              Census Reporter was built using <span className="bold">Django</span>,
+              <span className="bold"> Flask</span>, and <span className="bold">PostgreSQL</span>.
+            </p>
+            <div className="project-summary-icons">
+              <IconButton href="https://github.com/censusreporter/censusreporter">
+                <span className="fa fa-github"></span>
+              </IconButton>
+              <IconButton href="https://github.com/censusreporter/census-api">
+                <span className="fa fa-github"></span>
+              </IconButton>
+            </div>
+          </ProjectDisplay>
+        </div>
 
-      <div className="small-project-display">
-        <ProjectDisplay title="Scholarships.com">
-          <p className="project-summary-text">
-            While interning for Scholarships.com, I overhauled their two affiliate sites: RightStudent
-            and ASM Omnichannel, as well as redesigned a few pages on Scholarships.com. Many of the
-            pages I worked on needed a fresh, modern redesign. Focus was placed on seamlessly
-            responsive layouts and extremely intuitive page designs. Contact me for more details.
-          </p>
-        </ProjectDisplay>
+        <div className="small-project-display">
+          <ProjectDisplay title="TeslaSpotting">
+            <p className="project-summary-text highlight">
+              Encouraging children to eat better.
+            </p>
 
-        <ProjectDisplay title="Superhero Fueling" img={SuperheroScreenshot}>
-          <p className="project-summary-text highlight">
-            Encouraging children to eat better.
-          </p>
-          <p className="project-summary-text">
-            Superhero Fueling is an app for children that gameifies their eating habits and
-            encourages them to eat more healthily. As they or their parents input the foods
-            they eat into the app, the superhero will either fly - and save the day - or fall,
-            depending on how healthy their choice of foods was.
-          </p>
-          <p className="project-summary-text">
-            This app was built in the second five weeks of the Northwestern University
-            <a href="https://www.cs.northwestern.edu/academics/courses/394/"> EECS 394 </a>
-            course as part of a four-person student developer team. Through working with
-            real client managers, we practiced <span className="bold">agile</span> methodologies
-            and learned about the product launch cycle.
-          </p>
-          <p className="project-summary-text">
-            Superhero Fueling was built using <span className="bold">Appgyver Supersonic </span>
-            and <span className="bold">Angular</span>.
-          </p>
-          <div className="project-summary-icons">
-            <IconButton href="https://github.com/eecs394-f16/Superhero-Fueling">
-              <span className="fa fa-github"></span>
-            </IconButton>
-          </div>
-        </ProjectDisplay>
+            <p className="project-summary-text">
+              Superhero Fueling was built using <span className="bold">Appgyver Supersonic </span>
+              and <span className="bold">Angular</span>.
+            </p>
 
-        <ProjectDisplay title="JoonPark.me (v1)">
-          <p className="project-summary-text">
-            The previous version of this website was designed to encompass the various
-            aspects of my life. The challenge was twofold: I needed a site design that would
-            showcase my skills and reflect my personality, and I wanted to fully utilize the power
-            of HTML and CSS3. I forced myself to use minimal JavaScript to build the site, allowing me
-            to focus on improving my HTML/CSS skills.
-          </p>
-          <p className="project-summary-text">
-            I quickly realized that I needed a more developer-centric portfolio website, and
-            redesigned the site as seen here incorporating elements from <a href="https://material.io/">Material Design</a>.
-          </p>
-          <div className="project-summary-icons">
-            <IconButton href="https://github.com/Joonpark13/joonpark.me-OLD">
-              <span className="fa fa-github"></span>
-            </IconButton>
-          </div>
-        </ProjectDisplay>
+            <div className="project-summary-icons">
+              <IconButton href="https://github.com/Joonpark13/tesla-spotting">
+                <span className="fa fa-github"></span>
+              </IconButton>
+            </div>
+          </ProjectDisplay>
 
-        <ProjectDisplay title="Felix Fernandez-Penny PR">
-          <p className="project-summary-text">Coming Soon</p>
-        </ProjectDisplay>
+          <ProjectDisplay title="Scholarships.com">
+            <p className="project-summary-text">
+              While interning for Scholarships.com, I overhauled their two affiliate sites: RightStudent
+              and ASM Omnichannel, as well as redesigned a few pages on Scholarships.com. Many of the
+              pages I worked on needed a fresh, modern redesign. Focus was placed on seamlessly
+              responsive layouts and extremely intuitive page designs. Contact me for more details.
+            </p>
+          </ProjectDisplay>
+
+          <ProjectDisplay title="Superhero Fueling" img={SuperheroScreenshot}>
+            <p className="project-summary-text highlight">
+              Encouraging children to eat better.
+            </p>
+            <p className="project-summary-text">
+              Superhero Fueling is an app for children that gameifies their eating habits and
+              encourages them to eat more healthily. As they or their parents input the foods
+              they eat into the app, the superhero will either fly - and save the day - or fall,
+              depending on how healthy their choice of foods was.
+            </p>
+            <p className="project-summary-text">
+              This app was built in the second five weeks of the Northwestern University
+              <a href="https://www.cs.northwestern.edu/academics/courses/394/"> EECS 394 </a>
+              course as part of a four-person student developer team. Through working with
+              real client managers, we practiced <span className="bold">agile</span> methodologies
+              and learned about the product launch cycle.
+            </p>
+            <p className="project-summary-text">
+              Superhero Fueling was built using <span className="bold">Appgyver Supersonic </span>
+              and <span className="bold">Angular</span>.
+            </p>
+            <div className="project-summary-icons">
+              <IconButton href="https://github.com/eecs394-f16/Superhero-Fueling">
+                <span className="fa fa-github"></span>
+              </IconButton>
+            </div>
+          </ProjectDisplay>
+
+          <ProjectDisplay title="JoonPark.me (v1)">
+            <p className="project-summary-text">
+              The previous version of this website was designed to encompass the various
+              aspects of my life. The challenge was twofold: I needed a site design that would
+              showcase my skills and reflect my personality, and I wanted to fully utilize the power
+              of HTML and CSS3. I forced myself to use minimal JavaScript to build the site, allowing me
+              to focus on improving my HTML/CSS skills.
+            </p>
+            <p className="project-summary-text">
+              I quickly realized that I needed a more developer-centric portfolio website, and
+              redesigned the site as seen here incorporating elements from <a href="https://material.io/">Material Design</a>.
+            </p>
+            <div className="project-summary-icons">
+              <IconButton href="https://github.com/Joonpark13/joonpark.me-OLD">
+                <span className="fa fa-github"></span>
+              </IconButton>
+            </div>
+          </ProjectDisplay>
+
+          <ProjectDisplay title="Felix Fernandez-Penny PR">
+            <p className="project-summary-text">Coming Soon</p>
+          </ProjectDisplay>
+        </div>
       </div>
     </ContentPage>
 
